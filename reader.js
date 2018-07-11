49fa11a992ee4d4b31ec6aaae0079e12d47f7824
Cryptosporidium <coolawesomedude10@gmail.com>
2018-07-10 22:05:03 -0700

    everything done for final release

--- reader.js
+++ reader.js
@@ -6,14 +6,14 @@ window.onload = check;
 
 function check() {
 	var urlmenu = document.getElementById( 'menu1' );
-	var small = documnet.getElementById("small");
-	var med = document.getElementById("med");
-	var large = document.getElementById("large");
-	var content = document.getElementById(
+	// var small = documnet.getElementById("small");
+	// var med = document.getElementById("med");
+	// var large = document.getElementById("large");
+	// var content = document.getElementById();
 	window.onchange = chapterSelect(urlmenu);
-	fontChange(small, "8pt");
-	fontChange(med, "12pt");
-	fontChange(large, "18pt");
+	//fontChange(small, "8pt");
+	//fontChange(med, "12pt");
+	//fontChange(large, "18pt");
 }
 
 function chapterSelect(urlmenu) {
@@ -22,10 +22,10 @@ function chapterSelect(urlmenu) {
  };
 }
 
-function fontChange(butt, size){
+/*function fontChange(butt, size){
 	butt.onclick = function(){
 		butt.style.fontSize = size;
 	}
-}
+}*/
 
 }) ();
